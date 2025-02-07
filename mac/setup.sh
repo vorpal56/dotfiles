583735c763d82cf09107883ef4e16d4e0615770a
vorpal56 <anthonysukadil1@gmail.com>
2025-02-07 00:19:23 -0500

    setup script for mac

---
+++ mac/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+brew bundle install --file Brewfile
+cp karabiner.json ~/.config/karabiner/
+
+# Removes the caps lock delay. Could also be in ~/Library/LaunchAgents/
+# Can also move to ~/Library/LaunchAgents/
+sudo cp local.removecapslockdelay.plist /Library/LaunchDaemons/
+
+# Turns on natural scrolling (so that it's inverted, swipe up is down, swipe down is up). Trackpad only thing?
+# Settings > Trackpad > Scroll & Zoom > Natural Scrolling is ON
+# defaults write -g com.apple.swipescrolldirection -bool true
+
+# Turns off the clicking wallpaper to reveal desktop
+# TODO: setting is wrong?
+# defaults write -g com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
+
+# Minimizes the dock delay and animation speed
+defaults write com.apple.dock autohide-delay -float 0.01
+defaults write com.apple.dock autohide-time-modifier -float 0.2
+killall Dock
+
+source ~/.zshrc
